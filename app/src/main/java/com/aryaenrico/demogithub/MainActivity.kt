2fdf5e9415f7628db6feca88c996c929e6e59f54
Arya Enrico <aryaenrico789@gmail.com>
2022-05-12 20:32:35 +0700

    initial commit

---
+++ app/src/main/java/com/aryaenrico/demogithub/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.aryaenrico.demogithub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    private fun data (){
+
+    }
+}
